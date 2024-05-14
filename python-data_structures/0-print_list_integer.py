176ce15e4a7b2b276ec5608b08b8422a655741d1
Alex <alexccv94@gmail.com>
2024-05-13 23:54:30 -0400

    test

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
+# 0-print_list_integer.py
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+
+def print_list_integer(my_list=[]):
+    """Print all list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
