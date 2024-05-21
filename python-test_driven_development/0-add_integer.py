283ec463cec937bf215fc0b70c34138aac819bcc
Alex <alexccv94@gmail.com>
2024-05-20 23:08:46 -0400

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,24 @@
-#!/usr/bin/python3
-"""Module containing a dummy adder function for testing"""
-
-
 def add_integer(a, b=98):
-    """ adds integers
-        Arguments:
-        @a: first integer
-        @b: second integer, defaults to 98 if not given
     """
-
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    Adds two integers or floats. If the inputs are floats, they are cast to integers before addition.
+    
+    Args:
+    a: The first number, must be an integer or float.
+    b: The second number, must be an integer or float, defaults to 98.
+    
+    Returns:
+    The integer sum of a and b.
+    
+    Raises:
+    TypeError: If either a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    
+    # Cast to integers if they are floats
+    a = int(a)
+    b = int(b)
+    
+    return a + b
