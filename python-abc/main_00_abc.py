687428b068d1b73442b1837c754214be4e75dd93
Alex <alexccv94@gmail.com>
2024-05-24 16:18:13 -0400

    test

--- python-abc/main_00_abc.py
+++ python-abc/main_00_abc.py
@@ -3,3 +3,13 @@ from task_00_abc import Animal, Dog, Cat
 
 bobby = Dog()
 garfield = Cat()
+
+print(bobby.sound())  # Output: Bark
+print(garfield.sound())  # Output: Meow
+
+# The following line will raise a TypeError you cannot instantiate an abstract class
+try:
+    animal = Animal()
+    print(animal.sound())
+except TypeError as e:
+    print(e)  # Output: Can't instantiate abstract class Animal with abstract method sound
