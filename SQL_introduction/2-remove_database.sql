da2217545d78a3fa5ffb4b6e15cb68a16a60e2ba
Alex <alexccv94@gmail.com>
2024-06-17 22:43:02 -0400

    testing

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,22 +1,2 @@
-#!/bin/bash
-
-# Script to delete the database hbtn_0c_0 in your MySQL server.
-# If the database hbtn_0c_0 doesn’t exist, the script will not fail.
-# You are not allowed to use the SELECT or SHOW statements.
-
-# MySQL credentials
-# Replace 'your_mysql_username' and 'your_mysql_password' with your actual MySQL username and password
-USER="your_mysql_username"
-PASSWORD="your_mysql_password"
-HOST="localhost"
-
-# Name of the database to be dropped
-DATABASE="hbtn_0c_0"
-
-# Command to drop the database if it exists
-# The -u option specifies the MySQL username
-# The -p option specifies the MySQL password
-# The -h option specifies the MySQL host
-# The -e option allows you to pass the SQL command to be executed
-
-mysql --user="$USER" --password="$PASSWORD" --host="$HOST" --execute="DROP DATABASE IF EXISTS $DATABASE;"
+-- Deletes the database hbtn_0c_0.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
