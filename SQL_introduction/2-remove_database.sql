2e69b18b496ae12ee2bdf3614c1877b124485448
Alex <alexccv94@gmail.com>
2024-06-17 22:21:05 -0400

    testing

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # MySQL credentials
+# Replace 'your_mysql_username' and 'your_mysql_password' with your actual MySQL username and password
 USER="your_mysql_username"
 PASSWORD="your_mysql_password"
 HOST="localhost"
 
-# Database to be dropped
+# Name of the database to be dropped
 DATABASE="hbtn_0c_0"
 
 # Command to drop the database if it exists
+# The -u option specifies the MySQL username
+# The -p option specifies the MySQL password
+# The -h option specifies the MySQL host
+# The -e option allows you to pass the SQL command to be executed
 mysql -u"$USER" -p"$PASSWORD" -h"$HOST" -e "DROP DATABASE IF EXISTS $DATABASE;"
